3f3236b021af978f59f87780ea373f3c19f05337
Alyssa Nickow <alyssa@nickow.com>
2017-07-05 20:18:20 -0500

    updated while following along with lessons

--- test_templates/server.py
+++ test_templates/server.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
-
 @app.route('/')
-
-def hello_world():
-    return render_template('index.html', name = 'Jay')
-
+def index():
+    return render_template('index.html', phrase = 'hello', times = 5)
 app.run(debug=True)
