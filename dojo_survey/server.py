d620812708eff3192cbea0bc517d3a09f245875e
Alyssa Nickow <alyssa@nickow.com>
2017-07-07 14:50:48 -0500

    updated Dojo Survey functionality to make use of sessions and basic data validations

--- dojo_survey/server.py
+++ dojo_survey/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, flash, session
 
 app = Flask(__name__)
+app.secret_key = "secretkey"
 
 @app.route('/')
 def index():
@@ -9,6 +10,26 @@ def index():
 
 @app.route('/results', methods=['POST'])
 def results():
-    return render_template('results.html', name = request.form['name'], location = request.form['location'], language = request.form['language'], comment = request.form['comment'])
+    error = False
+    if len(request.form['name']) < 1 or len(request.form['comment']) < 1 :
+        error = True
+        flash("Fields cannot be left blank!")
+    if len(request.form['comment']) > 120:
+        error = True
+        flash("Please keep comments to 120 characters or less")
+    print error
+    if error:
+        return redirect('/')
+    else:
+        session['name']=request.form['name']
+        session['location']=request.form['location']
+        session['language']=request.form['language']
+        session['comment']=request.form['comment']
+        return redirect('/results_page')
+    
+    
+@app.route('/results_page')    
+def results_page():
+    return render_template('results.html', name = session['name'], location = session['location'], language = session['language'], comment = session['comment'])
 
 app.run(debug=True)
